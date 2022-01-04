05f3b294bf4703a2c01921f90851ab3495f0e943
Stefano Ortisi <stefanoortisi@gmail.com>
2022-01-03 19:51:43 -0700

    updated our menu-items to navigate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 
+const HatsPage = () => (
+  <div className="hats">
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <HomePage />
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
